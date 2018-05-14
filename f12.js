3f74c449842f1ddb30f7a93e6d4b57fae8d02c6e
tuan2308 <tntuan2308@gmail.com>
2018-05-14 17:43:45 +0700

    Update f12.js

--- f12.js
+++ f12.js
@@ -1,1 +1,3 @@
-link code: https://www.startuanit.net/2015/10/code-f12-facebook-tong-hop-da-chuc-nang.html
+link code: http://123link.pw/K5HV2g87
+
+https://www.startuanit.net/2015/10/code-f12-facebook-tong-hop-da-chuc-nang.html
