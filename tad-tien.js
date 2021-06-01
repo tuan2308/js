5fb7b18eaf2a25096f368fe8262cbbb293e0bf12
tuan2308 <tntuan2308@gmail.com>
2021-06-01 10:55:53 +0700

    Update tad-tien.js

--- tad-tien.js
+++ tad-tien.js
@@ -1,8 +1,11 @@
-$(".tad-tien").on("focus", function() {
-    $(this).val("")
-    console.log("srfd")
-});
-$(".tad-tien").on("blur", function() {
-    var a = $(this).val().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-    $(this).val("₫" + a)
+$( document ).ready(function() {
+    $(".tad-tien").on("focus", function() {
+        $(this).val("")
+        console.log("srfd")
+    });
+    $(".tad-tien").on("blur", function() {
+        var a = $(this).val().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+        $(this).val("₫" + a)
+    });
+      
 });
