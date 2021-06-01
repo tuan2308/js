2315a93f490dd8fb54ba29353d78fdf46aaad0e6
tuan2308 <tntuan2308@gmail.com>
2021-06-01 15:12:49 +0700

    zzz

--- tad/al.js
+++ tad/al.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
 var tm = "https://cdn." + document.location.hostname.slice(4, 17) + "/tad/"
 
-    $("head").append("<script src='" + tm + "html2can.js'>");
+    $("head").append("<script src='" + tm + "tv/h2.js'>");
     if ($(".btn-chonanh").length !== 0) {
         $("head").append("<link href='" + tm + "tad-crop.css' rel='stylesheet'/>");
         $("head").append("<script src='" + tm + "cr.js'>");
-        $("head").append("<script src='" + tm + "cropper.js'>");
+        $("head").append("<script src='" + tm + "tv/cr.js'>");
         $("body").append("<input class='cropanh d-none' value=''>")
         $("#cropanh").click(function() {
             $("#tad-taoanh").removeClass("disabled");
