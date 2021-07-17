1900ff656500a514100f83d1264a9f94a2b3a5bc
tuan2308 <tntuan2308@gmail.com>
2021-07-17 12:22:37 +0700

    a

--- tad/effect/sadphone.js
+++ tad/effect/sadphone.js
@@ -1,67 +1,41 @@
-$(document).ready(function() {
-    if (window.location.hostname == tmbq) {
-        $("head").append(cssCrop);
+$(document).ready(function() { if (window.location.hostname == tmbq) { $("head").append(cssCrop);
         $("head").append(jsCrop);
-        $("head").append(jqueryCrop);
-        var b = document.getElementById("Nimg");
-        var e = document.getElementById("Cimg");
-        var j = new Image;
-        j.crossOrigin = "Anonymous";
-        j.src = bgimg;
-        var o = new Image;
-        o.crossOrigin = "Anonymous";
-        o.src = limg;
-        var a = 0;
-        var d = vitri;
-        $("#tad-taoanh").click(function() {
-            j.src = bgimg;
-            o.src = limg;
-            d = vitri;
+        $("head").append(jqueryCrop); var g = document.getElementById("Nimg"); var k = document.getElementById("Cimg"); var i = new Image;
+        i.crossOrigin = "Anonymous";
+        i.src = bgimg; var n = new Image;
+        n.crossOrigin = "Anonymous";
+        n.src = limg; var h = 0; var l = vitri;
+        $("#tad-taoanh").click(function() { i.src = bgimg;
+            n.src = limg;
+            l = vitri;
             $("#img-out").hide();
             $("#tad-taoanh").addClass("disabled").html("<span class='spinner-border spinner-border-sm'></span> Đang tạo ảnh");
-            setTimeout(function() { j.onload = m() }, 200)
-        });
+            setTimeout(function() { i.onload = f() }, 200) });
 
-        function m() {
-            c = document.querySelector("canvas"), ctx = c.getContext("2d"), corners = d, step = 1;
-            var q, h, n, r, p, i, f, l, s = e.width - 1,
-                A = e.height - 1;
+        function f() { c = document.querySelector("canvas"), ctx = c.getContext("2d"), corners = l, step = 1; var b, u, j, a, d, t, w, m, z = k.width - 1,
+                e = k.height - 1;
             ctx.clearRect(0, 0, c.width, c.height);
             ctx.save();
-            ctx.drawImage(j, 0, 0, c.width, c.height);
+            ctx.drawImage(i, 0, 0, c.width, c.height);
             ctx.restore();
+            ctx.save(); for (y = 0; y < e; y += step) { for (x = 0; x < z; x += step) { d = v(corners[0], corners[3], y / e);
+                    t = v(corners[1], corners[2], y / e);
+                    w = v(corners[0], corners[3], (y + step) / e);
+                    m = v(corners[1], corners[2], (y + step) / e);
+                    b = v(d, t, x / z);
+                    u = v(d, t, (x + step) / z);
+                    j = v(w, m, (x + step) / z);
+                    a = v(w, m, x / z);
+                    ctx.drawImage(k, x, y, step, step, b.x, b.y, Math.ceil(Math.max(step, Math.abs(u.x - b.x), Math.abs(a.x - j.x))) + 1, Math.ceil(Math.max(step, Math.abs(b.y - a.y), Math.abs(u.y - j.y))) + 1) } }
             ctx.save();
-            for (y = 0; y < A; y += step) {
-                for (x = 0; x < s; x += step) {
-                    p = g(corners[0], corners[3], y / A);
-                    i = g(corners[1], corners[2], y / A);
-                    f = g(corners[0], corners[3], (y + step) / A);
-                    l = g(corners[1], corners[2], (y + step) / A);
-                    q = g(p, i, x / s);
-                    h = g(p, i, (x + step) / s);
-                    n = g(f, l, (x + step) / s);
-                    r = g(f, l, x / s);
-                    ctx.drawImage(e, x, y, step, step, q.x, q.y, Math.ceil(Math.max(step, Math.abs(h.x - q.x), Math.abs(r.x - n.x))) + 1, Math.ceil(Math.max(step, Math.abs(q.y - r.y), Math.abs(h.y - n.y))) + 1)
-                }
-            }
-            ctx.save();
-            ctx.drawImage(o, 0, 0, c.width, c.height);
-            ctx.restore();
-            if (b) {
-                ctx.save();
-                var k = ctx.createPattern(b, "repeat");
-                ctx.fillStyle = k;
+            ctx.drawImage(n, 0, 0, c.width, c.height);
+            ctx.restore(); if (g) { ctx.save(); var o = ctx.createPattern(g, "repeat");
+                ctx.fillStyle = o;
                 ctx.fillRect(0, 0, c.width, c.height);
-                ctx.restore()
-            }
-            c.toBlob(function(t) {
-                var u = URL.createObjectURL(t);
-                $("#img-out").html("<label class='mt-2'> Kết quả: </label><img src='" + u + "' alt='Tạo ảnh đẹp' class='img-thumbnail'></img><a href='" + u + "' class='btn btn-block btn-primary mt-2' download='taoanhdep_" + tenanhxuat + "'><i class='fas fa-cloud-download-alt'></i> Tải ảnh về</a>");
+                ctx.restore() }
+            c.toBlob(function(q) { var p = URL.createObjectURL(q);
+                $("#img-out").html("<label class='mt-2'> Kết quả: </label><img src='" + p + "' alt='Tạo ảnh đẹp' class='img-thumbnail'></img><a href='" + p + "' class='btn btn-block btn-primary mt-2' download='taoanhdep_" + tenanhxuat + "'><i class='fas fa-cloud-download-alt'></i> Tải ảnh về</a>");
                 $("#tad-taoanh").removeClass("disabled").html("<i class='fas fa-cut'></i> Tạo ảnh");
-                $("#img-out").show()
-            }, 'image/jpeg');
+                $("#img-out").show() }, "image/jpeg");
 
-            function g(v, u, t) { return { x: v.x + (u.x - v.x) * t, y: v.y + (u.y - v.y) * t } }
-        }
-    }
-});
+            function v(p, q, r) { return { x: p.x + (q.x - p.x) * r, y: p.y + (q.y - p.y) * r } } } } });
--- tad/tv/mk.js
+++ tad/tv/mk.js
@@ -1,82 +1,46 @@
-$(document).ready(function() {
-    if (window.location.hostname == tmbq) {
-        $("head").append(cssCrop);
+$(document).ready(function() { if (window.location.hostname == tmbq) { $("head").append(cssCrop);
         $("head").append(jsCrop);
         $("head").append(jqueryCrop);
-        $("body").append("<img src='https://1.bp.blogspot.com/-z3RZ2G93tVo/YHES6yUdnzI/AAAAAAAArsw/Jq_pD24Pn-sEW-XwzGl18qlgIYQZZOeqgCNcBGAsYHQ/s0/blank.png' crossorigin='anonymous' id='Bimg' class='d-none'/> <img src='https://1.bp.blogspot.com/-z3RZ2G93tVo/YHES6yUdnzI/AAAAAAAArsw/Jq_pD24Pn-sEW-XwzGl18qlgIYQZZOeqgCNcBGAsYHQ/s0/blank.png' crossorigin='anonymous' id='Mimg' class='d-none'/>");
-        var j = document.getElementById("Nimg");
-        var b = document.getElementById("Cimg");
-        var d = document.getElementById("Bimg");
-        var o = document.getElementById("Mimg");
-        var e = 0;
-        var a = vitri;
+        $("body").append("<img src='https://1.bp.blogspot.com/-z3RZ2G93tVo/YHES6yUdnzI/AAAAAAAArsw/Jq_pD24Pn-sEW-XwzGl18qlgIYQZZOeqgCNcBGAsYHQ/s0/blank.png' crossorigin='anonymous' id='Bimg' class='d-none'/> <img src='https://1.bp.blogspot.com/-z3RZ2G93tVo/YHES6yUdnzI/AAAAAAAArsw/Jq_pD24Pn-sEW-XwzGl18qlgIYQZZOeqgCNcBGAsYHQ/s0/blank.png' crossorigin='anonymous' id='Mimg' class='d-none'/>"); var i = document.getElementById("Nimg"); var g = document.getElementById("Cimg"); var n = document.getElementById("Bimg"); var l = document.getElementById("Mimg"); var k = 0; var h = vitri;
         $("#Bimg").attr("src", bgimg);
         $("#Mimg").attr("src", markimg);
-        $("#tad-taoanh").click(function() {
-            $("#Bimg").attr("src", bgimg);
+        $("#tad-taoanh").click(function() { $("#Bimg").attr("src", bgimg);
             $("#Mimg").attr("src", markimg);
-            a = vitri;
+            h = vitri;
             $("#img-out").hide();
             $("#tad-taoanh").addClass("disabled").html("<span class='spinner-border spinner-border-sm'></span> Đang tạo ảnh");
-            setTimeout(function() {
-                d.onload = m()
-            }, 200)
-        });
+            setTimeout(function() { n.onload = f() }, 200) });
 
-        function m() {
-            c = document.querySelector("canvas"), ctx = c.getContext("2d"), corners = a, step = 1;
-            var n, s, h, p, k, A, q, g, l = b.width - 1,
-                i = b.height - 1;
+        function f() { c = document.querySelector("canvas"), ctx = c.getContext("2d"), corners = h, step = 1; var j, z, u, d, o, e, b, v, m = g.width - 1,
+                t = g.height - 1;
             ctx.clearRect(0, 0, c.width, c.height);
-            ctx.save();
-            for (y = 0; y < i; y += step) {
-                for (x = 0; x < l; x += step) {
-                    k = r(corners[0], corners[3], y / i);
-                    A = r(corners[1], corners[2], y / i);
-                    q = r(corners[0], corners[3], (y + step) / i);
-                    g = r(corners[1], corners[2], (y + step) / i);
-                    n = r(k, A, x / l);
-                    s = r(k, A, (x + step) / l);
-                    h = r(q, g, (x + step) / l);
-                    p = r(q, g, x / l);
-                    ctx.drawImage(b, x, y, step, step, n.x, n.y, Math.ceil(Math.max(step, Math.abs(s.x - n.x), Math.abs(p.x - h.x))) + 1, Math.ceil(Math.max(step, Math.abs(n.y - p.y), Math.abs(s.y - h.y))) + 1)
-                }
-            }
-            if (tadMk == "black") {
-                ctx.save();
+            ctx.save(); for (y = 0; y < t; y += step) { for (x = 0; x < m; x += step) { o = a(corners[0], corners[3], y / t);
+                    e = a(corners[1], corners[2], y / t);
+                    b = a(corners[0], corners[3], (y + step) / t);
+                    v = a(corners[1], corners[2], (y + step) / t);
+                    j = a(o, e, x / m);
+                    z = a(o, e, (x + step) / m);
+                    u = a(b, v, (x + step) / m);
+                    d = a(b, v, x / m);
+                    ctx.drawImage(g, x, y, step, step, j.x, j.y, Math.ceil(Math.max(step, Math.abs(z.x - j.x), Math.abs(d.x - u.x))) + 1, Math.ceil(Math.max(step, Math.abs(j.y - d.y), Math.abs(z.y - u.y))) + 1) } } if (tadMk == "black") { ctx.save();
                 ctx.globalAlpha = 0.3;
                 ctx.fillStyle = "#333";
                 ctx.fillRect(0, 0, c.width, c.height);
-                ctx.restore()
-            }
+                ctx.restore() }
             ctx.globalCompositeOperation = "destination-in";
-            ctx.drawImage(o, 0, 0, c.width, c.height);
+            ctx.drawImage(l, 0, 0, c.width, c.height);
             ctx.restore();
             ctx.save();
             ctx.globalCompositeOperation = "destination-over";
-            ctx.drawImage(d, 0, 0, c.width, c.height);
-            ctx.restore();
-            if (j) {
-                ctx.save();
-                ctx.globalAlpha = 0.5;
-                var f = ctx.createPattern(j, "repeat");
-                ctx.fillStyle = f;
+            ctx.drawImage(n, 0, 0, c.width, c.height);
+            ctx.restore(); if (i) { ctx.save();
+                ctx.globalAlpha = 0.5; var w = ctx.createPattern(i, "repeat");
+                ctx.fillStyle = w;
                 ctx.fillRect(0, 0, c.width, c.height);
-                ctx.restore()
-            }
-            c.toBlob(function(t) {
-                var u = URL.createObjectURL(t);
-                $("#img-out").html("<label class='mt-2'> Kết quả: </label><img src='" + u + "' alt='Tạo ảnh đẹp' class='img-thumbnail'></img><a href='" + u + "' class='btn btn-block btn-primary mt-2'  download='taoanhdep_" + tenanhxuat + "'><i class='fas fa-cloud-download-alt'></i> Tải ảnh về</a>");
+                ctx.restore() }
+            c.toBlob(function(q) { var p = URL.createObjectURL(q);
+                $("#img-out").html("<label class='mt-2'> Kết quả: </label><img src='" + p + "' alt='Tạo ảnh đẹp' class='img-thumbnail'></img><a href='" + p + "' class='btn btn-block btn-primary mt-2'  download='taoanhdep_" + tenanhxuat + "'><i class='fas fa-cloud-download-alt'></i> Tải ảnh về</a>");
                 $("#tad-taoanh").removeClass("disabled").html("<i class='fas fa-cut'></i> Tạo ảnh");
-                $("#img-out").show()
-            }, 'image/jpeg');
+                $("#img-out").show() }, "image/jpeg");
 
-            function r(v, u, t) {
-                return {
-                    x: v.x + (u.x - v.x) * t,
-                    y: v.y + (u.y - v.y) * t
-                }
-            }
-        }
-    }
-});
+            function a(p, q, r) { return { x: p.x + (q.x - p.x) * r, y: p.y + (q.y - p.y) * r } } } } });
